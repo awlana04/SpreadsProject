55ca680f76ffeddf86bea8d2a41f04c72137e090
Greyson <greysonf@gmail.com>
2020-12-06 20:11:15 -0300

    List pagination

--- backend/src/controllers/ConcludeController.ts
+++ backend/src/controllers/ConcludeController.ts
@@ -1,21 +1,49 @@
 import { Request, Response } from 'express';
 import knex from '../database/connection';
+import { attachPaginate } from 'knex-paginate'
+
+attachPaginate();
+interface IPaginateParams {
+  perPage: number,
+  currentPage: number,
+  isFromStart?: boolean,
+  isLengthAware?: boolean,
+}
+
+interface IWithPagination<T = any> {
+  data: T;
+  pagination: IPagination;
+}
+
+interface IPagination {
+  total?: number;
+  lastPage?: number;
+  currentPage: number;
+  perPage: number;
+  from: number;
+  to: number;
+}
 
 class ConcludeController {
   async index(request: Request, response: Response) {
     const supervisor: any = request.query.supervisor
     const promotor: any = request.query.promotor
+    const page: any = request.query.page
+    const perpage: any = request.query.perpage
 
     if (supervisor || promotor) {
       const data = await knex('conclude')
         .select('*')
         .where('supervisao_prisma', supervisor)
         .orWhere('promotor_prisma', promotor)
+        .paginate({ perPage: perpage, currentPage: page });
 
       return response.json(data);
     }
     else {
-      const data = await knex('conclude').select('*');
+      const data = await knex('conclude')
+        .select('*')
+        .paginate({ perPage: perpage, currentPage: page });
 
       return response.json(data);
     }
