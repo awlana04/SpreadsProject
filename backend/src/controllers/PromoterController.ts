8407e71ae7d514c9ad79be2fe1e2c8ab5053a080
Greyson <greysonf@gmail.com>
2020-12-06 21:43:44 -0300

    Fix promoter and supervisor list

--- backend/src/controllers/PromoterController.ts
+++ backend/src/controllers/PromoterController.ts
@@ -3,13 +3,29 @@ import knex from '../database/connection';
 
 class PromoterController {
   async index(request: Request, response: Response) {
-    const data = await knex('conclude')
-      .select('promotor_prisma')
-      .whereNotNull('promotor_prisma')
-      .orderBy('promotor_prisma')
-      .distinct('promotor_prisma')
+    const supervisor: any = request.query.supervisor
+    const promotor: any = request.query.promotor
 
-    return response.json(data);
+    if (supervisor) {
+      const data = await knex('conclude')
+        .select('promotor_prisma')
+        .where('supervisao_prisma', supervisor)
+        .whereNotNull('promotor_prisma')
+        .orderBy('promotor_prisma')
+        .distinct('promotor_prisma')
+
+      return response.json(data);
+    }
+    else {
+      const data = await knex('conclude')
+        .select('promotor_prisma')
+        .whereNotNull('promotor_prisma')
+        .orderBy('promotor_prisma')
+        .distinct('promotor_prisma')
+
+      return response.json(data);
+    }
+
   }
 
 }
--- backend/src/controllers/SupervisorController.ts
+++ backend/src/controllers/SupervisorController.ts
@@ -3,13 +3,27 @@ import knex from '../database/connection';
 
 class SupervisorController {
   async index(request: Request, response: Response) {
-    const data = await knex('conclude')
-      .select('supervisao_prisma')
-      .whereNotNull('supervisao_prisma')
-      .orderBy('supervisao_prisma')
-      .distinct('supervisao_prisma')
+    const promotor: any = request.query.promotor
 
-    return response.json(data);
+    if (promotor) {
+      const data = await knex('conclude')
+        .select('supervisao_prisma')
+        .whereNotNull('supervisao_prisma')
+        .where('promotor_prisma', promotor)
+        .orderBy('supervisao_prisma')
+        .distinct('supervisao_prisma')
+
+      return response.json(data);
+    }
+    else {
+      const data = await knex('conclude')
+        .select('supervisao_prisma')
+        .whereNotNull('supervisao_prisma')
+        .orderBy('supervisao_prisma')
+        .distinct('supervisao_prisma')
+
+      return response.json(data);
+    }
   }
 
 }
